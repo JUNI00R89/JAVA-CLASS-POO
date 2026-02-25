3f8a080a43954839e7a47b0a1d6f6f585b8bc388
frank <frankjunior5400@gmail.com>
2026-02-24 22:09:09 -0500

    intento 100

---
+++ java/com/example/Main.java
@@ -0,0 +1,35 @@
+package com.example;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+  Producto Producto = new Producto(); 
+   
+ Producto.setPrecio(4.000);  
+ Producto.nombreProducto = "Vitafer";
+ Producto.setidProducto(2); 
+ Producto.Stock = 45; 
+
+
+
+
+  System.out.println(Producto.toString());
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
