e54bb38e1858c8be95fecade8c461368f254f035
Frank junior benitez mosquera <frankjunior5400@gmail.com>
2026-02-26 07:19:57 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,54 @@
+package com.example;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    //    Personas Personas = new Personas();
+
+    //     Personas.colorOjos = "Negro";
+    //     Personas.colorPiel = "Blanco"; 
+    //     Personas.tallaZapato = 40 ; 
+    //     Personas.tallaCamisa = "S"; 
+
+
+    //     System.out.println("el color de los ojos es : " + Personas.colorOjos);
+    //     System.out.println("el color de los ojos es : " + Personas.colorPiel);
+    //     System.out.println("Su talla de camisa es  : " + Personas.tallaCamisa);
+    //     System.out.println("Su talla de zapato es : " + Personas.tallaZapato);
+
+
+
+  Producto Producto = new Producto(); 
+   
+ Producto.Precio = 4.000;  
+ Producto.NombreProducto = "Manzana";
+ Producto.idProducto = 1; 
+ Producto.Stock = 45; 
+
+
+
+
+  System.out.println(Producto.toString());
+
+
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
